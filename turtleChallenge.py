36388706e537093ce46059cc4bd60e8396f47dfe
unknown <adel.mekki1998@hotmail.com>
2019-12-20 11:53:01 +0100

    Correction et ajout verif pronostique

--- turtleChallenge.py
+++ turtleChallenge.py
@@ -54,13 +54,13 @@ J2_list = []
 prediction_J1 = -1
 prediction_J2 = -1
 for i in range(1, 4):
-    prediction_J1 = input("J1, veuillez saisir votre prédiction pour la  %i eme place" % (i))
+    prediction_J1 = int(input("J1, veuillez saisir votre prédiction pour la  %i eme place" % (i)))
     J1_list.append(prediction_J1)
 print("Merci J1")
 print(J1_list)
 
 for i in range(1, 4):
-    prediction_J2 = input("J2, veuillez saisir votre prédiction pour la %i eme place" % (i))
+    prediction_J2 = int(input("J2, veuillez saisir votre prédiction pour la %i eme place" % (i)))
     J2_list.append(prediction_J2)
 print("Merci J2")
 print(J2_list)
@@ -73,50 +73,62 @@ splinter_speed = random.randint(1, 5)
 donatello_speed = random.randint(1, 5)
 
 course_lancee = True
+resultats_course = []
 while course_lancee:
 
     if(michelangelo.position()[0] > 700):
-        michelangelo.forward(0)
+        if(1 not in resultats_course):
+            resultats_course.append(1)
+
     else:
         michelangelo.forward(michelangelo_speed)
 
     if (leonardo.position()[0] > 700):
-        leonardo.forward(0)
+        if(2 not in resultats_course):
+            resultats_course.append(2)
+
     else:
         leonardo.forward(leonardo_speed)
 
     if (raphael.position()[0] > 700):
-        raphael.forward(0)
+        if(3 not in resultats_course):
+            resultats_course.append(3)
     else:
         raphael.forward(raphael_speed)
 
     if (splinter.position()[0] > 700):
-        splinter.forward(0)
+        if(4 not in resultats_course):
+            resultats_course.append(4)
     else:
         splinter.forward(splinter_speed)
 
     if (donatello.position()[0] > 700):
-        donatello.forward(0)
+        if(5 not in resultats_course):
+            resultats_course.append(5)
     else:
         donatello.forward((donatello_speed))
 
     if(donatello.position()[0] > 700 and splinter.position()[0] > 700 and raphael.position()[0] > 700 and leonardo.position()[0] > 700 and michelangelo.position()[0] > 700):
         print("La course est terminée")
         break
-resultats_list = [michelangelo.position(), leonardo.position(), raphael.position(), splinter.position(), donatello.position()]
-print(resultats_list)
+
+print(resultats_course)
 # Comparer les rÃ©sultats de la course avec les pronostics des joueurs
 # et afficher le rÃ©sultat de la course
 # et le joueur gagnant avec la tortue arbitre et l'instruction turtle.Write Ã  la position 0,0
 # A IMPLEMENTER
-# PAS EU LE TEMPS :/
 
 
 turtle_arbitre = turtle.Turtle()
 turtle_arbitre.goto(0,0)
 turtle_arbitre.color("Black")
-turtle_arbitre.write("Le joueur 1 Ã  gagnÃ©", move=True, align="left", font=("Arial", 16, "normal"))
+turtle_arbitre.write("Le joueur %i a gagné !" % (resultats_course[0]), move=True, align="left", font=("Arial", 16, "normal"))
 
-
+print(J1_list)
+print(resultats_course[:3])
+if(J1_list == resultats_course[:3]):
+    print("Les pronostiques du J1 sont bons ! bravo !")
+if(J2_list == resultats_course[:3]):
+    print("Les pronostiques du J2 sont bons ! bravo !")
 
 turtle.mainloop()
